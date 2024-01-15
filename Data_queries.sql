230a378d8cf317082ae2c77737c9378cd429e731
Samuel Oyedele <68781050+Samizic29@users.noreply.github.com>
2024-01-15 12:40:47 +0100

    Create Data_queries.sql

---
+++ Data_queries.sql
@@ -0,0 +1,4 @@
+/*
+Danny's Diner business data analysis
+Skills used: Basic Aggregations, Windows Function, Sub-query, Common Table Expression (CTE), Joins, etc. was used to solve the business questions.
+*/
